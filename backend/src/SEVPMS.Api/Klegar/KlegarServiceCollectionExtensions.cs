80b6e20e59ed75f1300d25bb8544d8c78f28180b
Klegyoag <280890308+Klegyoag@users.noreply.github.com>
2026-09-01 18:28:38 +0530

    feat(klegar): register seating layout backend services

--- backend/src/SEVPMS.Api/Klegar/KlegarServiceCollectionExtensions.cs
+++ backend/src/SEVPMS.Api/Klegar/KlegarServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-using SEVPMS.Application.Features.Seats.Interfaces;
+﻿using SEVPMS.Application.Features.Seats.Interfaces;
 using SEVPMS.Application.Features.Seats.Services;
 using SEVPMS.Application.Features.Tickets.Interfaces;
 using SEVPMS.Application.Features.Tickets.Services;
@@ -11,11 +11,14 @@ public static class KlegarServiceCollectionExtensions
     public static IServiceCollection AddKlegarBackend(this IServiceCollection services)
     {
         services.AddSingleton(TimeProvider.System);
-        services.AddScoped<ISeatInventoryRepository, EfSeatInventoryRepository>(); services.AddScoped<ITicketRepository, EfTicketRepository>();
-        services.AddScoped<ISeatService, SeatService>(); services.AddScoped<ITicketService, TicketService>();
+        services.AddScoped<ISeatInventoryRepository, EfSeatInventoryRepository>();
+        services.AddScoped<ISeatingLayoutRepository, EfSeatingLayoutRepository>(); services.AddScoped<ITicketRepository, EfTicketRepository>();
+        services.AddScoped<ISeatService, SeatService>();
+        services.AddScoped<ISeatingLayoutService, SeatingLayoutService>(); services.AddScoped<ITicketService, TicketService>();
         services.AddSingleton<ITicketQrTokenService, HmacTicketQrTokenService>();
         services.AddScoped<KlegarRealtimeNotifier>(); services.AddScoped<ISeatRealtimeNotifier>(sp => sp.GetRequiredService<KlegarRealtimeNotifier>()); services.AddScoped<ITicketRealtimeNotifier>(sp => sp.GetRequiredService<KlegarRealtimeNotifier>());
         services.AddScoped<RequestUserResolver>();
         return services;
     }
 }
+
