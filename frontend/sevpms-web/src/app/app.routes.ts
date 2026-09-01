4a7ab85bf65f9ba7ff1f5a3cb84021644eb26f5a
Umesha <thiyagarajaumesha@gmail.com>
2026-09-01 14:25:31 +0530

    feat: configure initial application routes

--- frontend/sevpms-web/src/app/app.routes.ts
+++ frontend/sevpms-web/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/admin/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'admin/dashboard',
+    component: Dashboard
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
